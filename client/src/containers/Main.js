dacbfb6775f7cd1f371466bc6570962e209f06a6
Luis Quinones <alexx813@gmail.com>
2018-03-27 20:07:53 -0500

    last commit

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,38 +1,72 @@
-import React from 'react';
-import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
-import { connect } from 'react-redux';
-import Homepage from '../components/Homepage';
-import AuthForm from '../components/AuthForm';
+import React from "react";
+import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
+import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors";
+import withAuth from "../hocs/withAuth";
+import MessageForm from "../containers/MessageForm";
 
 const Main = props => {
-  return(
+  const { authUser, errors, removeError, currentUser } = props;
+  return (
     <div className="container">
-       <Switch>
-         <Route exact path="/" render={props => <Homepage {...props}/>} />
-         <Route exact path="/signin" render={props => {
-            return(
-              <AuthForm  buttonText="Log in" heading="Welcome Back."{...props} />
-            )
-         }
-        } />
-         <Route exact path="/signup" render={props => {
-            return(
-              <AuthForm  
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => <Homepage currentUser={currentUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/signin"
+          render={props => {
+            return (
+              <AuthForm
+                removeError={removeError}
+                errors={errors}
+                onAuth={authUser}
+                buttonText="Log in"
+                heading="Welcome Back."
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => {
+            return (
+              <AuthForm
+                removeError={removeError}
+                errors={errors}
+                onAuth={authUser}
                 signUp
-                buttonText="Sign me up" heading="Join Warbler Today."{...props} />
-            )
-         }
-        } />
-       </Switch>
+                buttonText="Sign me up!"
+                heading="Join Warbler today."
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          path="/users/:id/messages/new"
+          component={withAuth(MessageForm)}
+        />
+      </Switch>
     </div>
   );
 };
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-
-    currentUser: state.currentUser
-  }
+    currentUser: state.currentUser,
+    errors: state.errors
+  };
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(
+  connect(mapStateToProps, { authUser, removeError })(Main)
+);
