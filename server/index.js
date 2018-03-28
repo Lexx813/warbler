0d263fe7b125c4157b3eec5d19449b0a65f0b9f5
Luis Quinones <alexx813@gmail.com>
2018-03-28 11:03:33 -0500

    changed port

--- server/index.js
+++ server/index.js
@@ -43,17 +43,20 @@ app.use(function(req, res, next) {
 
 app.use(errorHandler);
 
-// SERVER SETUP //
 if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('../client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require('path');
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+//this is for server
 
-const port = process.env.PORT || 8081
-
-app.listen(port, () => {
-  console.log(`SERVER STARTED AT PORT ${port}`);
-
-});
+const PORT = process.env.PORT || 8081;
+app.listen(PORT);
+console.log('SERVER STARTED');
