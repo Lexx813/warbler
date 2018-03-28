dad855de2d9c66edadf941de46a33d9bad197e6e
Luis Quinones <alexx813@gmail.com>
2018-03-28 11:03:58 -0500

    changed

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === 'production') {
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
 //this is for server
